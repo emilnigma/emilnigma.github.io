0e88e8e00186c300b2cab1196934049f1fb47f54
emilnigma <enigm@web.de>
2025-06-24 19:46:59 +0200

    prune assets ingredients

--- src/Assets/Ingredients.ts
+++ src/Assets/Ingredients.ts
@@ -1,5 +1,3 @@
-import Store from '../Core/Store';
-
 type IngredientAsset = Record<string, {
   img: string
   description: string
@@ -8,19 +6,13 @@ type IngredientAsset = Record<string, {
   afterPickEffect?: () => void
 }>;
 
-const effectBase = () => {
-  const store = Store.getInstance();
-  const { player } = store;
-  return { store, player };
-};
-
 const ingredients: IngredientAsset = {
   'Fire Lily': {
     img: 'assets/ingredients/fire_lily.jpg',
     description: 'Fire Lilies are volatile and highly flamable. Safely handling them earns you Gold, but using over seven will blow up your cauldron.',
     instantEffect: () => {
-      const { player } = effectBase();
-      player.changeCurrency({ gold: 1 });
+      // const { player } = effectBase();
+      // player.changeCurrency({ gold: 1 });
     },
   },
   'Humble Carrot': {
@@ -31,47 +23,47 @@ const ingredients: IngredientAsset = {
     img: 'assets/ingredients/blue_trumpet_mushrooms.jpg',
     description: 'Mushrooms move an extra spot if there is a carrot in your cauldron. Two spots if there is three or more carrots.',
     instantEffect: () => {
-      const { player } = effectBase();
-      const carrots = player.chipsOnBoard.filter((c) => (c.chip.kind === 'Humble Carrot')).reduce((sum) => sum + 1, 0);
-      const [mushroom] = player.getHighestChipOnBoard();
-      if (carrots > 2) { mushroom.position += 2; return; }
-      if (carrots > 0) { mushroom.position += 1; }
+      // const { player } = effectBase();
+      // const carrots = player.chipsOnBoard.filter((c) => (c.chip.kind === 'Humble Carrot')).reduce((sum) => sum + 1, 0);
+      // const [mushroom] = player.getHighestChipOnBoard();
+      // if (carrots > 2) { mushroom.position += 2; return; }
+      // if (carrots > 0) { mushroom.position += 1; }
     },
   },
   'Clover Leaf': {
     img: 'assets/ingredients/clover_leaf.jpg',
     description: 'If you are lucky and the Clover Leaf is your penultimate or last Ingredient Card you receive an extra gem.',
-    condition: () => {
-      const { player } = effectBase();
-      const highestChips = player.getHighestChipOnBoard();
-      return highestChips[0].chip.kind === 'Clover Leaf'
-       || (highestChips.length > 1 && highestChips[1].chip.kind === 'Clover Leaf');
-    },
+    // condition: () => {
+    //   // const { player } = effectBase();
+    //   // const highestChips = player.getHighestChipOnBoard();
+    //   // return highestChips[0].chip.kind === 'Clover Leaf'
+    //   //  || (highestChips.length > 1 && highestChips[1].chip.kind === 'Clover Leaf');
+    // },
     afterPickEffect: () => {
-      const { player } = effectBase();
-      const highestChips = player.getHighestChipOnBoard();
-      if (highestChips[0].chip.kind === 'Clover Leaf') player.changeCurrency({ emerald: 1 });
-      if (highestChips.length > 1 && highestChips[1].chip.kind === 'Clover Leaf') player.changeCurrency({ emerald: 1 });
+      // const { player } = effectBase();
+      // const highestChips = player.getHighestChipOnBoard();
+      // if (highestChips[0].chip.kind === 'Clover Leaf') player.changeCurrency({ emerald: 1 });
+      // if (highestChips.length > 1 && highestChips[1].chip.kind === 'Clover Leaf') player.changeCurrency({ emerald: 1 });
     },
   },
   'Sliverlake Seaweed': {
     img: 'assets/ingredients/sliverlake_seaweed.jpg',
     description: 'The seaweed found in Sliverlake holds magical water. The first occurence of seaweed refreshes your Dilution.',
     instantEffect: () => {
-      const { player } = effectBase();
-      const [, ...otherChips] = player.getHighestChipOnBoard();
-      const alreadyHasSeaweed = otherChips.some(({ chip }) => chip.kind === 'Sliverlake Seaweed');
-      if (player.effects.hasDilute || alreadyHasSeaweed) return;
-      player.setProps({ effects: { hasDilute: true } });
+      // const { player } = effectBase();
+      // const [, ...otherChips] = player.getHighestChipOnBoard();
+      // const alreadyHasSeaweed = otherChips.some(({ chip }) => chip.kind === 'Sliverlake Seaweed');
+      // if (player.effects.hasDilute || alreadyHasSeaweed) return;
+      // player.setProps({ effects: { hasDilute: true } });
     },
   },
   'Golden Fish Scale': {
     img: 'assets/ingredients/salmon.jpg',
     description: 'Shiny scales help you haggle with other merchants. Per scale you receive 30% off your next purchase.',
     instantEffect: () => {
-      const { player } = effectBase();
-      const { isHaggling = 0 } = player.effects;
-      player.setProps({ effects: { isHaggling: isHaggling + 1 } });
+      // const { player } = effectBase();
+      // const { isHaggling = 0 } = player.effects;
+      // player.setProps({ effects: { isHaggling: isHaggling + 1 } });
     },
   },
   'Spider Silk': {
